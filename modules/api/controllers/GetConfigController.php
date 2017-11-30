03e0771724387475f81943e749d02fc6168fd089
xin <137105161@qq.com>
2017-11-30 14:27:20 +0800

    1.修改接口接受变动

--- modules/api/controllers/GetConfigController.php
+++ modules/api/controllers/GetConfigController.php
@@ -1,14 +1,24 @@
 <?php
 namespace app\modules\api\controllers;
 
-use app\models\AppConfig;
 use yii\web\Controller;
+use app\models\AppConfig;
 
 class GetConfigController extends Controller
 {
     public function actionIndex($url)
     {
-        $model = $this->findModel($url);
+        $model = AppConfig::find()
+            ->joinWith('publicConfig')
+            ->where(['domain' => $url, 'type' => AppConfig::TYPE_ENTRANCE])
+            ->one();
+
+        if ($model->status == AppConfig::STATUS_ERROR) {
+            $model = AppConfig::find()
+                ->joinWith('publicConfig')
+                ->where(['type' => AppConfig::TYPE_ENTRANCE, 'status' => AppConfig::STATUS_SUCCESS, 'public_config.activity_id' => $model->publicConfig->activity_id])
+                ->one();
+        }
 
         return json_encode([
             'code' => 0,
@@ -23,7 +33,7 @@ class GetConfigController extends Controller
     public function findModel($url)
     {
         $url = strip_tags(trim($url));
-        if (($model = AppConfig::find()->joinWith('publicConfig')->where(['domain' => $url])->one()) == null) {
+        if (($model = AppConfig::find()->joinWith('publicConfig')->where(['domain' => $url, 'type' => AppConfig::TYPE_ENTRANCE])->one()) == null) {
             return json_encode([
                 'code' => -1,
                 'msg' => 'error'
